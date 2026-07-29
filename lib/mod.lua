bc83c02f266c02b15bbcf8c65fded7ccbec9f17b
sonocircuit <49308219+sonocircuit@users.noreply.github.com>
2026-07-29 21:48:11 +0200

    add post system hook

--- lib/mod.lua
+++ lib/mod.lua
@@ -1,15 +1,13 @@
 local mod = require 'core/mods'
 
+mod.hook.register("system_post_startup", "fx mod post startup", function()
+  osc.send({ "localhost", 57120 }, "/fxmod/alloc");
+end)
+
 mod.hook.register("script_pre_init", "fx mod pre init", function()
-    osc.send(
-        { "localhost", 57120 },
-        "/fxmod/init",
-        {});
+  osc.send({ "localhost", 57120 }, "/fxmod/init");
 end)
 
 mod.hook.register("script_post_cleanup", "fx mod post cleanup", function()
-    osc.send(
-        { "localhost", 57120 },
-        "/fxmod/cleanup",
-        {});
+  osc.send({ "localhost", 57120 }, "/fxmod/cleanup");
 end)
