3051343f4129bd9508e443375c2c91b74f2daa35
sonocircuit <49308219+sonocircuit@users.noreply.github.com>
2026-08-06 22:37:34 +0200

    add fx separator

--- lib/mod.lua
+++ lib/mod.lua
@@ -1,11 +1,19 @@
 local mod = require 'core/mods'
 
+local function add_params()
+  params:add_separator("fx_mod", "fx")
+end
+
 mod.hook.register("system_post_startup", "fx mod post startup", function()
   osc.send({ "localhost", 57120 }, "/fxmod/alloc");
 end)
 
 mod.hook.register("script_pre_init", "fx mod pre init", function()
   osc.send({ "localhost", 57120 }, "/fxmod/init");
+end)
+
+mod.hook.register("script_post_init", "_fx mod post init", function()
+  add_params()
 end)
 
 mod.hook.register("script_post_cleanup", "fx mod post cleanup", function()
