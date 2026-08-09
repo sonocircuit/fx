1031e43533f0e989d90c4eedd45d2793acd3be36
sonocircuit <49308219+sonocircuit@users.noreply.github.com>
2026-08-09 02:15:10 +0200

    move bus allocation to pre init

--- lib/mod.lua
+++ lib/mod.lua
@@ -4,15 +4,12 @@ local function add_params()
   params:add_separator("fx_mod", "fx")
 end
 
-mod.hook.register("system_post_startup", "fx mod post startup", function()
+mod.hook.register("script_pre_init", "fx mod pre init", function()
   osc.send({ "localhost", 57120 }, "/fxmod/alloc");
 end)
 
-mod.hook.register("script_pre_init", "fx mod pre init", function()
-  osc.send({ "localhost", 57120 }, "/fxmod/init");
-end)
-
 mod.hook.register("script_post_init", "_fx mod post init", function()
+  osc.send({ "localhost", 57120 }, "/fxmod/init");
   add_params()
 end)
 
